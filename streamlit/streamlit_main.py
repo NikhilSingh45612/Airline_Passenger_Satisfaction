4e0053f3d568769978402654a56070cb7cb46b3d
NikhilSingh45612 <99918755+NikhilSingh45612@users.noreply.github.com>
2022-06-15 03:25:21 +0530

    Update streamlit_main.py

--- streamlit/streamlit_main.py
+++ streamlit/streamlit_main.py
@@ -45,7 +45,7 @@ import streamlit as st
 import seaborn as sns
 
 # Importing data
-data = pd.read_csv(r'airline_passenger_satisfaction.csv', index_col=False)
+data = pd.read_csv(r'https://github.com/NikhilSingh45612/Airline_Passenger_Satisfaction/blob/994f534e1e50f4dfa0e1880ddb6058a45ef25bce/notebook/airline_passenger_satisfaction.csv', index_col=False)
 
 
 
