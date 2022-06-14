05e2d75c121937cc51bf143f3a096fc8abfc9ff4
NikhilSingh45612 <99918755+NikhilSingh45612@users.noreply.github.com>
2022-06-15 01:29:57 +0530

    Update airline_satisfaction_main.py

--- streamlit/airline_satisfaction_main.py
+++ streamlit/airline_satisfaction_main.py
@@ -40,7 +40,7 @@ intro_herramientas_fuentes = '''
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import streamlit as st
 import seaborn as sns
 
